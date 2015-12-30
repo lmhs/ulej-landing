64a066356b6e1ff0b2ed78d221cfe431c899146c
Maksim Łabovič <maksim.labovicz@gmail.com>
2015-12-30 12:26:53 +0300

    layout and header

--- gulp/styles.js
+++ gulp/styles.js
@@ -4,12 +4,19 @@ import gutil from 'gulp-util';
 import gulpif from 'gulp-if';
 import rupture from 'rupture';
 import stylus from 'gulp-stylus';
-import autoprefixer from 'autoprefixer-stylus';
+// import autoprefixer from 'autoprefixer-stylus';
+
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import cssgrace from 'cssgrace';
+import rgbaFallback from 'postcss-color-rgba-fallback';
+import fontWeight from 'postcss-font-weights';
 import gcmq from 'gulp-group-css-media-queries';
 import minifyCss from 'gulp-minify-css';
 import csscomb from 'gulp-csscomb';
 import rename from 'gulp-rename';
 import errorHandler from 'gulp-plumber-error-handler';
+import { browsers } from '../package.json';
 
 gulp.task('styles', () => (
 	gulp.src('*.styl', {
@@ -19,14 +26,29 @@ gulp.task('styles', () => (
 		.pipe(plumber({errorHandler: errorHandler('Error in \'styles\' task')}))
 		.pipe(stylus({
 			use: [
-				rupture(),
-				autoprefixer()
+				rupture()
+				// ,
+				// autoprefixer()
 			],
 			sourcemap: !!gutil.env.debug && {
 				comment: false,
 				inline: true
 			}
 		}))
+		.pipe(postcss([
+			rgbaFallback(),
+			autoprefixer(
+					'Android >= ' + browsers.android,
+					'Chrome >= ' + browsers.chrome,
+					'Firefox >= ' + browsers.firefox,
+					'Explorer >= ' + browsers.ie,
+					'iOS >= ' + browsers.ios,
+					'Opera >= ' + browsers.opera,
+					'Safari >= ' + browsers.safari
+				),
+			fontWeight()
+			]
+		))
 		.pipe(gulpif(!gutil.env.debug, gcmq()))
 		.pipe(gulpif(!gutil.env.debug, minifyCss()))
 		.pipe(gulpif(gutil.env.csscomb, csscomb()))
